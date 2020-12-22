fca2813d03711e3a2b224aaa0d1b97147335cf0a
Anders Eknert <anders@eknert.com>
2020-12-22 21:56:26 +0100

    Force destroy bucket

--- s3.tf
+++ s3.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "opa_lab" {
-  bucket = var.bucket_name
-  acl    = "private"
+  bucket        = var.bucket_name
+  acl           = "private"
+  force_destroy = true
 
   tags = {
     Name = "OPA lab bucket"
